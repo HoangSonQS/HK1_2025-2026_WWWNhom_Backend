1777b511ddc6cd29cf8ab3ee41c8ceb295c596ef
Hoang Son <sonjiny2004@gmail.com>
2025-10-03 23:22:47 +0700

    feat: complete phase 1 configuration (PostgreSQL, Security, Email, Static Resources)

---
+++ src/main/java/iuh/fit/se/sebook_backend/SeBookBackendApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.sebook_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeBookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeBookBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iuh/fit/se/sebook_backend/SeBookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.sebook_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeBookBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
