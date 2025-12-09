89b33ac9e645ab58e21ab38ee402043e3d5aedf3
Hoang Son <sonjiny2004@gmail.com>
2025-12-10 01:31:07 +0700

    adding mail service

--- src/main/java/iuh/fit/se/sebook_backend/configs/SecurityConfig.java
+++ src/main/java/iuh/fit/se/sebook_backend/configs/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000", "https://hk-1-2025-2026-www-nhom-frontend.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
